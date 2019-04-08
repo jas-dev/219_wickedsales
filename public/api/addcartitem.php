c9ee72a05df5620073e08bdbadc0187bab6c312f
Daniel Paschal <daniel.paschal@learningfuze.com>
2019-04-08 11:18:54 -0700

    updated cart add for existing cart added session startup added session tester file added check for product id grabbing product id from query string check for cart id in session grabbing cart id from session storing cart id in session update query for updating product count

--- public/api/addcartitem.php
+++ public/api/addcartitem.php
@@ -8,7 +8,11 @@ require_once('config.php');
 
 require_once('mysqlconnect.php');
 
-$product_id = 1;
+if(empty($_GET['product_id'])){
+	throw new Exception('You must send a product_id (int) with your request');
+}
+
+$product_id = intval($_GET['product_id']);
 $product_quantity = 1;
 $user_id = 1;
 
@@ -30,7 +34,7 @@ $product_price = (int)$product_data['price'];
 
 $product_total = $product_price * $product_quantity;
 
-if(empty($cart_id)){
+if(empty($_SESSION['cart_id'])){
 	$cart_create_query = "INSERT INTO `carts` SET 
 		`item_count` = $product_quantity,
 		`total_price` = $product_total,
@@ -46,12 +50,17 @@ if(empty($cart_id)){
 		throw new Exception('data was not added to cart table');
 	}
 	$cart_id = mysqli_insert_id($conn);
+	$_SESSION['cart_id'] = $cart_id;
+} else {
+	$cart_id = $_SESSION['cart_id'];
 }
 
 $cart_item_query = "INSERT INTO `cart_items` SET
 	`products_id`= $product_id,
 	`quantity` = $product_quantity,
 	`carts_id` = $cart_id
+	ON DUPLICATE KEY UPDATE 
+	`quantity` = `quantity` + $product_quantity
 ";
 
 $cart_item_result = mysqli_query($conn, $cart_item_query);
