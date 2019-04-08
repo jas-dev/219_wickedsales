093203167ce71d265c136e743a0be85dc7406084
Scott Bowler <scott.bowler@learningfuze.com>
2019-04-08 16:09:46 -0700

    Completed product details page

--- src/components/products/product_details.js
+++ src/components/products/product_details.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import ProductCarousel from './product_carousel';
+import MiscDetails from './misc_details';
+import { formatMoney } from '../../helpers';
 
 class ProductDetails extends Component {
     state = {
@@ -29,7 +31,6 @@ class ProductDetails extends Component {
     }
 
     render(){
-        console.log('Product Details:', this.state.details);
         const { details } = this.state;
 
         if(details === null){
@@ -38,13 +39,34 @@ class ProductDetails extends Component {
             return <h1 className="center">No Product Found</h1>
         }
 
-        const { description = 'No description available', images, name } = details;
+        const { description = 'No description available', images, miscDetails, name, price } = details;
 
         return (
             <div className="product-details">
                 <h1 className="center">{name}</h1>
-                <ProductCarousel images={images}/>
-                <p>{description}</p>
+                <div className="row">
+                    <ProductCarousel images={images} />
+                    <div className="col s12 m8">
+                        <div className="right-align product-price">{formatMoney(price)}</div>
+                        <div className="right-align add-to-cart">
+                            <span className="qty-container">
+                                <button className="btn btn-small btn-floating purple lighten-1">
+                                    <i className="material-icons">remove</i>
+                                </button>
+                                <span className="product-qty">1</span>
+                                <button className="btn btn-small btn-floating purple lighten-1">
+                                    <i className="material-icons">add</i>
+                                </button>
+                            </span>
+                            
+                            <button className="btn purple darken-2">
+                                <i className="material-icons">add_shopping_cart</i>
+                            </button>
+                        </div>
+                        <p>{description}</p>
+                        <MiscDetails details={miscDetails}/>
+                    </div>
+                </div>
             </div>
         );
     }
