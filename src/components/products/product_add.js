e4bc948011f5c7324f58ca0c0b8c8e1b757dc1aa
jas-dev <jaysamora@outlook.com>
2019-04-09 18:02:24 -0700

    client can add items to cart, increment and decrement items to add

---
+++ src/components/products/product_add.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+
+class ProductAdd extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            qty: 1
+        };
+
+        this.incrementQty=this.incrementQty.bind(this);
+        this.decrementQty=this.decrementQty.bind(this);
+        this.addToCart=this.addToCart.bind(this);
+    }
+    incrementQty(){
+        this.setState({
+            qty: this.state.qty+1
+        })
+    }
+    decrementQty(){
+        if(this.state.qty >1){
+            this.setState({
+                qty: this.state.qty-1
+            })
+        }
+
+    }
+    addToCart(){
+        console.log('add', this.state.qty, 'product to cart ID:', this.props.productId)
+    }
+    render(){
+        return(
+            <div className="right-align add-to-cart">
+                <span className="qty-container">
+                    <button className="btn btn-small btn-floating purple lighten-1">
+                        <i className="material-icons" onClick={this.decrementQty}>remove</i>
+                    </button>
+                    <span className="product-qty">{this.state.qty}</span>
+                    <button className="btn btn-small btn-floating purple lighten-1">
+                        <i className="material-icons" onClick={this.incrementQty}>add</i>
+                    </button>
+                </span>
+
+                <button className="btn purple darken-2">
+                    <i className="material-icons" onClick={this.addToCart}>add_shopping_cart</i>
+                </button>
+            </div>
+
+        )
+    }
+}
+
+export default ProductAdd;
