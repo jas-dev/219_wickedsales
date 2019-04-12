7634c1fea428b64391020505d83478a1735232fc
jas-dev <jaysamora@outlook.com>
2019-04-12 16:55:07 -0700

    created logout endpoint with validation and error handling

---
+++ public/api/logout.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once('functions.php');
+
+set_exception_handler('handleError');
+
+require_once('config.php');
+
+require_once('mysqlconnect.php');
+
+$output = [
+    'success'=> false,
+    'message'=> null
+];
+
+$id = $_SESSION['user_data']['id'];
+$token =$_SESSION['user_data']['token'];
+$username =$_SESSION['user_data']['username'];
+
+$delete_query = "DELETE FROM `user_connections` WHERE `token`=$token";
+
+$result = mysqli_query($conn,$delete_query);
+
+if(empty($_SESSION['user_data']['token'])){
+    $output['success']= true;
+    $output['message']= 'you were not logged in';
+    print(json_encode($output));
+    exit();
+}
+
+if(!$result){
+    throw new Exception(mysqli_error($conn));
+}
+
+if(mysqli_affected_rows($conn) !== 1) {
+    throw new Exception('unable to logout');
+}
+
+$output['success']= true;
+
+unset($_SESSION['user_data']);
+
+$json_output= json_encode($output);
+
+print_r($json_output);
+
+?>
