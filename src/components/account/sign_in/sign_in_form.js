e659a4e04d29fbb17748c04b3003bd740abb1b01
jas-dev <jaysamora@outlook.com>
2019-04-17 11:54:56 -0700

    started redux form

--- src/components/account/sign_in/sign_in_form.js
+++ src/components/account/sign_in/sign_in_form.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import {reduxForm} from 'redux-form';
+import {reduxForm, Field} from 'redux-form';
 
 const SignInForm = props =>{
+
+    console.log('sign in form props:', props)
     return(
         <form>
-            <h1> Form up in here</h1>
+            <div className='input-field'>
+                <Field id='email' name='email' component='input' type='text'/>
+                <label htmlFor='email'>Email</label>
+            </div>
         </form>
     );
 };
