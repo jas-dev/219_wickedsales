884b9350da3080f86f88844d25a1ed523a3adaf4
jas-dev <jaysamora@outlook.com>
2019-04-25 17:36:35 -0700

    created higher order component

--- src/components/account/account_routes.js
+++ src/components/account/account_routes.js
@@ -3,14 +3,22 @@ import {Route, Switch} from 'react-router-dom';
 import NotFound from '../404';
 import SignIn from './sign_in';
 import SignOut from './sign_out';
+/*import SignUp from './sign_up';*/
+import Orders from './orders';
+import Profile from './profile';
+import auth from '../../hoc/auth';
 
 export default props => {
     const { match }= props;
     console.log('match:', match);
     return (
         <Switch>
-            <Route path={`${match.path}/sign-in`} component={SignIn}/>
-            <Route path={`${match.path}/sign-out`} component={SignOut}/>
+            <Route path={`${match.path}/sign-in`} component={auth(SignIn, '/account/orders', false)}/>
+            <Route path={`${match.path}/sign-out`} component={auth(SignOut, '/')}/>
+            {/*<Route path={`${match.path}/sign-up`} component={auth(SignUp, '/products', false)}/>*/}
+            <Route path={`${match.path}/orders`} component={auth(Orders)}/>
+            <Route path={`${match.path}/profile`} component={auth(Profile)}/>
+
             <Route component={NotFound}/>
         </Switch>
     )
